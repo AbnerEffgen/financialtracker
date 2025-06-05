cce2869bb7293c3dd5c5b6f94901734ac78d9a6e
AbnerEffgen <abnereffgen@hotmail.com>
2025-06-04 21:03:01 -0300

    primeiro-commit

---
+++ financialtracker/src/main/java/com/abner/financialtracker/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.abner.financialtracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Ta funcionando ate aqui";
+    }
+}
