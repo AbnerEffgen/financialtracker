2a80b92a28c38f035cb1c60370b6dbc071bac867
AbnerEffgen <abnereffgen@hotmail.com>
2025-06-14 14:22:24 -0300

    criacao classe Transaction

---
+++ financialtracker/src/main/java/com/abner/financialtracker/model/Transaction.java
@@ -0,0 +1,52 @@
+package com.abner.financialtracker.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "transaction")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type; // REVENUE OR EXPENSE
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    private String description;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public enum TransactionType {
+        REVENUE,
+        EXPENSE
+    }
+}
